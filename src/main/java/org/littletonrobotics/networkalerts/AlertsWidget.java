9396aa71bc0969fa7badb047f8d794e5167d7e00
Ido <idoben2@hotmail.com>
2023-06-17 19:39:27 -0700

    Combined extender and claw into one image

--- src/main/java/org/littletonrobotics/networkalerts/AlertsWidget.java
+++ src/main/java/org/littletonrobotics/networkalerts/AlertsWidget.java
@@ -4,6 +4,7 @@ import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -19,7 +20,18 @@ import javafx.scene.transform.Rotate;
 @Description(name = "AlertsIdo", dataTypes = Alerts.class, summary = "Displays a list of alerts.")
 @ParametrizedController("AlertsWidget.fxml")
 public final class AlertsWidget extends SimpleAnnotatedWidget<Alerts> {
-  private Image _armClawClosedImage = new Image(getClass().getResourceAsStream("img/ArmClawClosed.png"));
+  private final double m_scaleImages = 0.25;
+  private double m_tileWidth;
+  private double m_tileHeight;
+
+  private final Image m_armExtenderImage = new Image(getClass().getResourceAsStream(
+      "img/ArmExtender.png"));
+  private final Image m_armClawClosedImage = new Image(getClass().getResourceAsStream(
+      "img/ArmClawClosed.png"));
+  private final Image m_armClawOpenedImage = new Image(getClass().getResourceAsStream(
+      "img/ArmClawOpen.png"));
+  private final double m_pxLeftClawOpen = 4;
+  private final double m_pxLeftClawClosed = 0;
 
   @FXML
   @SuppressWarnings("checkstyle:MemberNameCheck")
@@ -35,11 +47,51 @@ public final class AlertsWidget extends SimpleAnnotatedWidget<Alerts> {
     canvas.heightProperty().bind(root.heightProperty());
 
     // Redraw when size changes
-    canvas.widthProperty().addListener(evt -> drawRobotArm());
-    canvas.heightProperty().addListener(evt -> drawRobotArm());
+    canvas.widthProperty().addListener(evt -> redrawRobotArm());
+    canvas.heightProperty().addListener(evt -> redrawRobotArm());
   }
 
-  private Image calcResizedImage(Image image, double width, double height) {
+  private ImageView getScaledViewOfImage(Image img) {
+    ImageView view = new ImageView(img);
+    view.setFitWidth(img.getWidth() * m_scaleImages);
+    view.setFitHeight(img.getHeight() * m_scaleImages);
+    view.setSmooth(true);
+
+    return view;
+  }
+
+  private Image calcExtenderAndClawImage(boolean isClawOpen) {
+    Image clawToUse = isClawOpen
+        ? m_armClawOpenedImage :
+        m_armClawClosedImage;
+
+    // The open claw needs to be shifted left a few pixels, to avoid gap between it
+    // and the exxtender.
+    double pxLeftMarginClaw = isClawOpen
+        ? m_pxLeftClawOpen
+        : m_pxLeftClawClosed;
+
+    ImageView extenderView = getScaledViewOfImage(m_armExtenderImage);
+    ImageView clawView = getScaledViewOfImage(clawToUse);
+
+    double groupHeight = Math.max(
+        m_armExtenderImage.getHeight(),
+        clawToUse.getHeight());
+
+    // Combine image views
+    @SuppressWarnings("checkstyle:VariableDeclarationUsageDistance")
+    Group group = new Group(extenderView, clawView);
+    clawView.setLayoutX((m_armExtenderImage.getWidth() - pxLeftMarginClaw) * m_scaleImages);
+    clawView.setLayoutY((groupHeight - clawToUse.getHeight()) * m_scaleImages / 2);
+    extenderView.setLayoutY((groupHeight - m_armExtenderImage.getHeight()) * m_scaleImages / 2);
+
+    // Create snapshot of combined image
+    SnapshotParameters parameters = new SnapshotParameters();
+    parameters.setFill(Color.BLUE); // $TODO (Color.TRANSPARENT);
+    return group.snapshot(parameters, null);
+  }
+
+  private Image getResizedImage(Image image, double width, double height) {
     double pivotX = 167;
     double pivotY = 340;
 
@@ -61,34 +113,20 @@ public final class AlertsWidget extends SimpleAnnotatedWidget<Alerts> {
     return resizedImage;
   }
 
-  private void drawRobotArm() {
-    double width = root.getWidth();
-    double height = root.getHeight();
+  private void redrawRobotArm() {
+    m_tileWidth = root.getWidth();
+    m_tileHeight = root.getHeight();
 
-    if (width != 0 && height != 0) {
+    if (m_tileWidth != 0 && m_tileHeight != 0) {
       GraphicsContext gc = canvas.getGraphicsContext2D();
-      gc.clearRect(0, 0, width, height);
+      gc.clearRect(0, 0, m_tileWidth, m_tileHeight);
 
       // $TODO - I could be better about caching it
-      Image resizedImage = calcResizedImage(_armClawClosedImage, 523, 250);
+      Image resizedImage = calcExtenderAndClawImage(false);
 
-      double imageX = (width - resizedImage.getWidth()) / 2;
-      double imageY = (height - resizedImage.getHeight()) / 2;
+      double imageX = (m_tileWidth - resizedImage.getWidth()) / 2;
+      double imageY = (m_tileHeight - resizedImage.getHeight()) / 2;
       gc.drawImage(resizedImage, imageX, imageY);
-    }
-  }
-
-  private void drawLine() {
-    double width = root.getWidth();
-    double height = root.getHeight();
-
-    if (width != 0 && height != 0) {
-      GraphicsContext gc = canvas.getGraphicsContext2D();
-      gc.clearRect(0, 0, width, height);
-
-      gc.setStroke(Color.BLACK);
-      gc.setLineWidth(2);
-      gc.strokeLine(0, height / 2, width, height / 2);
     }
   }
 
